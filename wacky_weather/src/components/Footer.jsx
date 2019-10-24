4092931531cb139b7bc1f91b7f25baa360458a5f
grewa1 <gbjrmail@gmail.com>
2019-10-24 13:51:52 -0400

    added social media github

--- wacky_weather/src/components/Footer.jsx
+++ wacky_weather/src/components/Footer.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 export default function Footer() {
   return (
     <footer>
-      <p>&copy; 2019 &mdash; Gregoire Boisrond Jr</p>
+      <p>&copy; 2019 &mdash; Gregoire Boisrond Jr </p>
+      <a href="https://github.com/gregwa1" className="github-image"></a>
     </footer>
   )
 }
